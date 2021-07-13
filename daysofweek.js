6a4fc4de83e70cd87735ff39a488cb86a759496a
Piyush Shaw <piyushshaw8777@gmail.com>
2021-07-13 23:42:07 +0530

    added:selection practice problem with if else if and else

---
+++ daysofweek.js
@@ -0,0 +1,25 @@
+/********************************************************************
+ * Purpose - To read a number and display the week day 
+ * 
+ * @version 1.0
+ * @author Piyush
+ * @since 12-07-2021
+ *******************************************************************/
+
+
+let day = Math.floor(Math.random()*10)%7+1;
+console.log(day)
+if(day==1)
+    console.log("Monday");
+else if(day==2)
+    console.log("Tuesday");
+else if(day==3)
+    console.log("Wednesday");
+else if(day==4)
+    console.log("Thursday");
+else if(day==5)
+    console.log("Friday");
+else if(day==6)
+    console.log("Saturday");
+else if(day==7)
+    console.log("Sunday");
